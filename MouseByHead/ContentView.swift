a1e10769249007554aeaee4f0c8b9eff5fa74eef
pencilCool <yhtangcoder@gmail.com>
2025-05-04 03:08:56 +0800

    Initial Commit

---
+++ MouseByHead/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MouseByHead
+//
+//  Created by yuhua Tang on 2025/5/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MouseByHead/MouseByHeadApp.swift
@@ -0,0 +1,17 @@
+//
+//  MouseByHeadApp.swift
+//  MouseByHead
+//
+//  Created by yuhua Tang on 2025/5/4.
+//
+
+import SwiftUI
+
+@main
+struct MouseByHeadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
